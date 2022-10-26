9a29cc4e16efae6ab4e059a94e21c24f48cefa67
mathausdev <114772818+mathausdev@users.noreply.github.com>
2022-10-25 23:42:58 -0300

    Add files via upload

---
+++ 1-meuPrimeiroPrograma.js
@@ -0,0 +1,13 @@
+console.log ("Meu Primeiro Programa: trabalhando com com variaveis");
+
+const idade = 26;
+idade
+
+console.log ("idade");
+console.log (idade);
+console.log (idade+2);
+console.log (idade-2);
+console.log (idade/2);
+
+const idadeSomada = idade+2;
+console.log (idadeSomada);
